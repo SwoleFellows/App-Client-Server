c737e4abef826b578d2120b40395983fbf894629
DEAN <dean.murphy90@gmail.com>
2017-12-21 14:41:15 -0800

    changed api url

--- public/scripts/routine.js
+++ public/scripts/routine.js
@@ -6,7 +6,7 @@ $('#nav-routine').hide();
 
 (function (module) {
 
-  let __API_URL__ = 'http://localhost:3000';
+  let __API_URL__ = 'https://swolefellows.herokuapp.com/';
 
   const Exercise = function (rdo) {//rdo: rawDataObj
     Object.keys(rdo).forEach(key => {
